a2a42d00511d9340e7b8789146ca5e41d06e8f27
Armykookie35 <mercyojogo@gmail.com>
2025-08-11 14:21:31 +0100

    ...

--- App.js
+++ App.js
@@ -3,27 +3,28 @@ import {
   Text,
   View,
   Image,
-  TextInput,
+  ScrollView,
   TouchableOpacity,
 } from "react-native";
 import React from "react";
 import profile from "./assets/ROSE.jpg";
+import travel from './assets/travel.png'
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#f2f2f2",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>App</Text>
-      <Image source={profile} style={styles.image} />
+    <View style={styles.container}>
+      <Text style={styles.text}>LET'S</Text>
+      <Text style={[styles.text,{fontSize:30,fontWeight:'bold'}]}>EXPLORE</Text>
+      <Text style={[styles.text,{fontSize:24}]}>THE WORLD</Text>
+      <Text style={styles.text}>
+        This is your assignment to be submitted on or before thursday , and
+        there is no room for excuses .Other assignments wil be passed across to
+        you
+      </Text>
       <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>CDM</Text>
+        <Text style={styles.buttonText}>ENTER</Text>
       </TouchableOpacity>
+      <Image source={travel} style={ styles.image} />
     </View>
   );
 };
@@ -32,21 +33,32 @@ export default App;
 
 const styles = StyleSheet.create({
   image: {
-    height: 200,
-    width: 200,
-    borderRadius: 20,
+    height: 350,
+    width: 350,
   },
   buttonText: {
-    color: 'white',
-    fontSize:15
+    color: "white",
+    fontSize: 15,
   },
   button: {
-    backgroundColor: 'green',
+    backgroundColor: "#425884",
     height: 50,
     width: 200,
     borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:20
-  }
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#87ceeb",
+    padding: 20,
+  },
+  text: {
+    color: "#425884",
+    fontSize: 15,
+    textAlign: "left",
+    marginTop: 5,
+  },
 });
