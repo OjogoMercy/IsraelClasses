90b57a5e95a15c36710e69498d586e52af35a873
Armykookie35 <mercyojogo@gmail.com>
2025-08-11 13:48:56 +0100

    ...

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
-import { StyleSheet, Text, View ,Image,TextInput,TouchableOpacity} from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import profile from "./assets/ROSE.jpg";
 
 const App = () => {
   return (
-    <View style={{flex:1,backgroundColor:'white'}}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#f2f2f2",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Text>App</Text>
-      <Image />
+      <Image source={profile} style={styles.image} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>CDM</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize:15
+  },
+  button: {
+    backgroundColor: 'green',
+    height: 50,
+    width: 200,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:20
+  }
+});
